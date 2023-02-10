d9947ab8e19431de09a7f3a50e25ad020a7ac6a2
S M Navin Durai <71096790+Navin3d@users.noreply.github.com>
2023-02-10 11:44:19 +0530

    Create Shape.java

---
+++ Arrays/Shape.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int patternNumber = sc.nextInt();
+	    char[][] design = new char[patternNumber][patternNumber];
+		for(int row = 0; row < patternNumber; row++)
+		    for(int col = 0; col < patternNumber; col++)
+		        if((row + col) >= patternNumber-1)
+		            design[row][col] = '*';
+		        else {
+		            design[row][col] = '+';
+		        }
+		
+		for(int row = 0; row < patternNumber; row++) {
+		    for(int col = 0; col < patternNumber; col++)
+		        System.out.print(design[row][col]);
+		    System.out.println();
+		}
+	}
+}
